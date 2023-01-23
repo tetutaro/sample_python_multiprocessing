eeb9f32e1cd68c8e6e8822844bc870e885208133
Tetsutaro Maruyama <tetsutaro.maruyama@gmail.com>
2023-01-24 06:12:48 +0900

    v0.1.2

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,3 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
-__version__ = "0.1.1"  # Automatically updated by poetry-dynamic-versioning
+__version__ = "0.1.2"  # Automatically updated by poetry-dynamic-versioning
